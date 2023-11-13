b986610bfecb1ab3477ec4c7993766bbdb55d40e
Alex Linkoff <alink94@gmail.com>
2023-11-13 11:20:22 -0500

    Update ResultList.tsx

--- components/ResultList.tsx
+++ components/ResultList.tsx
@@ -3,33 +3,56 @@ import React from 'react';
 
 interface ResultListProps {
   results: any[];
-  onResultClick: (index: number) => void;
+  onResultClick: (resultIndex: number) => void;
 }
 
 const ResultList: React.FC<ResultListProps> = ({ results, onResultClick }) => {
+  // Aggregate results by ruleId and count occurrences
+  const aggregatedResults: { [ruleId: string]: { count: number; results: any[] } } = {};
+
+  results.forEach((result, index) => {
+    const ruleId = result.ruleId || 'N/A';
+
+    if (!aggregatedResults[ruleId]) {
+      aggregatedResults[ruleId] = { count: 0, results: [] };
+    }
+
+    aggregatedResults[ruleId].count++;
+    aggregatedResults[ruleId].results.push(result);
+  });
+
+  const sortedRuleIds = Object.keys(aggregatedResults).sort();
+
   return (
     <div>
-      <h2>Results List</h2>
-      <ul style={{ listStyle: 'none', padding: 0 }}>
-        {results.map((result, index) => (
-          <li
-            key={index}
-            onClick={() => onResultClick(index)}
-            style={{
-              cursor: 'pointer',
-              border: '1px solid #ccc',
-              padding: '10px',
-              margin: '5px',
-              borderRadius: '5px',
-              backgroundColor: '#f7f7f7',
-            }}
-          >
-            <strong>Rule ID:</strong> {result.ruleId}
-            <br />
-            <strong>Message:</strong> {result.message.text}
-          </li>
-        ))}
-      </ul>
+      <h2>High-Level Dashboard</h2>
+      {sortedRuleIds.map((ruleId) => (
+        <div
+          key={ruleId}
+          style={{ border: '1px solid #ccc', padding: '10px', margin: '10px', borderRadius: '5px', cursor: 'pointer' }}
+          onClick={() => onResultClick(results.findIndex((result) => result.ruleId === ruleId))}
+        >
+          <p>
+            <strong>Rule ID:</strong> {ruleId}
+          </p>
+          <p>
+            <strong>Occurrences:</strong> {aggregatedResults[ruleId].count}
+          </p>
+          <p>
+            <strong>Severity:</strong> {aggregatedResults[ruleId].results[0].level || 'N/A'}
+          </p>
+          <p>
+            <strong>Locations:</strong>
+            {aggregatedResults[ruleId].results.map((result, index) => (
+              <span key={index}>
+                {result.locations?.[0]?.physicalLocation?.artifactLocation?.uri}:{' '}
+                Line {result.locations?.[0]?.physicalLocation?.region?.startLine || 'N/A'}
+                {index < aggregatedResults[ruleId].results.length - 1 && ','}
+              </span>
+            ))}
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
