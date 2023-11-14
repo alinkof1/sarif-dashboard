55123670fd8a3b2cbddb5e2fb741a77001b38ebc
Alex Linkoff <alink94@gmail.com>
2023-11-14 14:34:39 -0500

    Update ResultList.tsx

--- components/ResultList.tsx
+++ components/ResultList.tsx
@@ -2,30 +2,43 @@
 import React, { useState, useEffect } from 'react';
 
 const getSeverity = (result: any): string => {
-  return result.runs && result.runs[0].rules && result.runs[0].rules[result.ruleId]
-    ? result.runs[0].rules[result.ruleId].defaultConfiguration.level
-    : 'Unknown';
+  if (result.runs && result.runs.length > 0 && result.runs[0].results) {
+    const ruleId = result.ruleId;
+    const rule = result.runs[0].rules.find((r: any) => r.id === ruleId);
+    if (rule && rule.defaultConfiguration) {
+      return rule.defaultConfiguration.level || 'Unknown';
+    }
+  }
+  return 'Unknown';
 };
 
-const ResultItem: React.FC<{ result: any; onClick: () => void; severityMapping: Record<string, string> }> = ({ result, onClick, severityMapping }) => {
-  return (
-    <div onClick={onClick} style={{ cursor: 'pointer', padding: '10px', borderBottom: '1px solid #ddd' }}>
-      <p>
-        <strong>Rule ID:</strong> {result.ruleId}
-      </p>
-      <p>
-        <strong>Severity:</strong> {getSeverity(result.ruleId, severityMapping)}
-      </p>
-      {/* Add more details as needed */}
-    </div>
-  );
-};
+const ResultList: React.FC<{ results: any[]; onResultClick: (resultIndex: number) => void }> = ({ results, onResultClick }) => {
+  const ruleOccurrences: Record<string, { occurrences: number; message: string }> = {};
 
-const ResultList: React.FC<{ results: any[]; onResultClick: (resultIndex: number) => void; severityMapping: Record<string, string> }> = ({ results, onResultClick, severityMapping }) => {
+  results.forEach((result) => {
+    const ruleId = result.ruleId;
+    ruleOccurrences[ruleId] = ruleOccurrences[ruleId] || { occurrences: 0, message: '' };
+    ruleOccurrences[ruleId].occurrences += 1;
+    ruleOccurrences[ruleId].message = result.message?.text || 'No message available';
+  });
+
   return (
     <div>
-      {results.map((result, index) => (
-        <ResultItem key={index} result={result} onClick={() => onResultClick(index)} severityMapping={severityMapping} />
+      {Object.keys(ruleOccurrences).map((ruleId) => (
+        <div key={ruleId} style={{ padding: '10px', borderBottom: '1px solid #ddd' }} onClick={() => onResultClick(-1)}>
+          <p>
+            <strong>Rule ID:</strong> {ruleId}
+          </p>
+          <p>
+            <strong>Severity:</strong> {getSeverity(results.find((result) => result.ruleId === ruleId) || {})}
+          </p>
+          <p>
+            <strong>Message:</strong> {ruleOccurrences[ruleId].message}
+          </p>
+          <p>
+            <strong>Occurrences:</strong> {ruleOccurrences[ruleId].occurrences}
+          </p>
+        </div>
       ))}
     </div>
   );
