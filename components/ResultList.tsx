fbb61c1afac126dbfd764e78fb2b21eaf6336d3b
Alex Linkoff <alink94@gmail.com>
2023-11-14 12:43:42 -0500

    Update ResultList.tsx

--- components/ResultList.tsx
+++ components/ResultList.tsx
@@ -1,35 +1,30 @@
 // components/ResultList.tsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-interface ResultListProps {
-  results: any[];
-  onResultClick: (index: number) => void;
-}
+const getSeverity = (ruleId: string, severityMapping: Record<string, string>): string => {
+  return severityMapping[ruleId] || 'Unknown';
+};
 
-const ResultList: React.FC<ResultListProps> = ({ results, onResultClick }) => {
+const ResultItem: React.FC<{ result: any; onClick: () => void; severityMapping: Record<string, string> }> = ({ result, onClick, severityMapping }) => {
+  return (
+    <div onClick={onClick} style={{ cursor: 'pointer', padding: '10px', borderBottom: '1px solid #ddd' }}>
+      <p>
+        <strong>Rule ID:</strong> {result.ruleId}
+      </p>
+      <p>
+        <strong>Severity:</strong> {getSeverity(result.ruleId, severityMapping)}
+      </p>
+      {/* Add more details as needed */}
+    </div>
+  );
+};
+
+const ResultList: React.FC<{ results: any[]; onResultClick: (resultIndex: number) => void; severityMapping: Record<string, string> }> = ({ results, onResultClick, severityMapping }) => {
   return (
     <div>
-      <h2>Results List</h2>
-      <ul style={{ listStyle: 'none', padding: 0 }}>
-        {results.map((result, index) => (
-          <li
-            key={index}
-            onClick={() => onResultClick(index)}
-            style={{
-              cursor: 'pointer',
-              border: '1px solid #ccc',
-              padding: '10px',
-              margin: '5px',
-              borderRadius: '5px',
-              backgroundColor: '#f7f7f7',
-            }}
-          >
-            <strong>Rule ID:</strong> {result.ruleId}
-            <br />
-            <strong>Message:</strong> {result.message.text}
-          </li>
-        ))}
-      </ul>
+      {results.map((result, index) => (
+        <ResultItem key={index} result={result} onClick={() => onResultClick(index)} severityMapping={severityMapping} />
+      ))}
     </div>
   );
 };
